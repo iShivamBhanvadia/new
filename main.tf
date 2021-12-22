e5709e8d1a8c32d27cc25750919e7c680c0329f4
ShivamBhanvadia <33751996+ShivamBhanvadia@users.noreply.github.com>
2021-12-22 20:37:18 +0530

    added

--- main.tf
+++ main.tf
@@ -20,12 +20,6 @@ variable "secret_access_key" {
 	description = "i3yC9fEbISDT/VBxvYDSMRzyN43uGKKoFYdvsqao"
 }
 
-variable "access_key" {
-	description = "AKIAWPYQ2ZYBN64EXDVC"
-}
-variable "secret_access_key" {
-	description = "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"
-}
 
 resource "aws_instance" "app_server" {
   ami           = "ami-0ed9277fb7eb570c9"
